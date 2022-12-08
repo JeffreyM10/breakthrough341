725cbcf9f831f727a719627a882fbca7bc38a2bd
Jeffrey Mazariegos <mazaje01@gettysburg.edu>
2022-12-07 19:01:52 -0500

    capture
    capture

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+//import 'package:breakthrough_state.dart';
+//import 'package:flutter/services.dart';
 
+import 'breakthrough_state.dart';
 
 final imageMap = {
-  Player.w: Image.asset('assets/white.png'),
-  Player.b: Image.asset('assets/black.png')
+  Piece.white: Image.asset("assets/white.png"),
+  Piece.black: Image.asset("assets/black.png")
 };
 
 void main() {
@@ -56,9 +59,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final _gameState = BreakthroughState();
-  int _counter = 0;
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -68,106 +68,80 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: ConstrainedBox(
-          const BoxConstraints(minWidth: 120.0, minHeight: 120.0),
-          child: AspectRatio(
-            aspectRatio: 5/6
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Flexible(
-                  child: AspectRatio(
-                        aspectRatio: 1 / 1,
-                        child: Stack(
-                          children: [
-                            Image.asset('assets/board.png'),
-                            GridView.builder(
-                              itemCount: BreakthroughState.numCells,
-                              gridDelegate:
-                                  const SliverGridDelegateWithFixedCrossAxisCount(
-                                        crossAxisCount: BreakthroughState.rows),
-                              itemBuilder: (context, index) {
-                                  return TextButton(
-                                    onPressed: () => _processPress(index),
-                                    child: imageMap[_gameState.board[index]] ??
-                                        Container(),
-                                  );
-                              })  
-                          ],
-                        )),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FittedBox(
-                            fit: BoxFit.fitWidth,
-                            child: Text(
-                              _gameState.getStatus(),
-                              style: const TextStyle(fontSize: 36),
-                            ),
-                          ),
-                          //reset button
-                          const Spacer(),
-                          ElevatedButton(
-                            onPressed: _resetGame,
-                            child: const Text(
-                              'Reset',
-                              style: TextStyle(fontSize: 36),
-                            ),
-                         )
-                       ])    
-            )
-              ]
-          ),
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minWidth: 120.0, minHeight: 120.0),
+            child: AspectRatio(
+              aspectRatio: 5 / 6,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      child: AspectRatio(
+                          aspectRatio: 1 / 1,
+                          child: Stack(
+                            children: [
+                              Image.asset('assets/board.png'),
+                              GridView.builder(
+                                  itemCount: BreakthroughState.numCells,
+                                  gridDelegate:
+                                      const SliverGridDelegateWithFixedCrossAxisCount(
+                                          crossAxisCount:
+                                              BreakthroughState.rows),
+                                  itemBuilder: (context, index) {
+                                    return TextButton(
+                                      onPressed: () => _processPress(index),
+                                      child:
+                                          imageMap[_gameState.board[index]] ??
+                                              Container(),
+                                    );
+                                  })
+                            ],
+                          )),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.all(10.0),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              FittedBox(
+                                fit: BoxFit.fitWidth,
+                                child: Text(
+                                  _gameState.getStatus(),
+                                  style: const TextStyle(fontSize: 36),
+                                ),
+                              ),
+                              //reset button
+                              const Spacer(),
+                              ElevatedButton(
+                                onPressed: _resetGame,
+                                child: const Text(
+                                  'Reset',
+                                  style: TextStyle(fontSize: 36),
+                                ),
+                              )
+                            ]))
+                  ]),
             ),
           ),
         ));
   }
 
-  void _getstatus(int index) {
-    setState((){
-      _gamesState.playAt(index);
+  void _processPress(int index) {
+    setState(() {
+      _gameState.playAt(index);
     });
   }
 
-
-void _resetGame() {
+  void _resetGame() {
     setState(() {
       _gameState.newGame();
     });
   }
-        
-        
-        
-        
-        
-        
-        
-        
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-      
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-
+}
